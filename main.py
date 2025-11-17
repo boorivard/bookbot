d22848263baf352e3b679bf51543b347cb3dbaa0
boorivard <boorivard@comcast.net>
2025-11-16 19:17:05 -0500

    refactoring get__num_words into stats.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
+from stats import get_num_words
+
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
-def get_num_words(book_text):
-    return
-
 def main():
     book_text = get_book_text("books/frankenstein.txt")
-    print(book_text)
+    num_words = get_num_words(book_text)
+    print(f"Found {num_words} total words")
 
 main()
