4b248c3911d58d75e838cdb67edb5f8eaa5fc59a
boorivard <boorivard@comcast.net>
2025-11-16 19:56:34 -0500

    added sorting_dict to stats.py, print_results to main.py, and plenty of comments

--- main.py
+++ main.py
@@ -1,16 +1,35 @@
 from stats import get_num_words
 from stats import get_num_char
+from stats import sort_dictionary
 
+#function that takes a file and converts the content of that file to a string
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
         file_contents = f.read()
     return file_contents
 
-def main():
-    book_text = get_book_text("books/frankenstein.txt")
-    num_words = get_num_words(book_text)
-    num_chars = get_num_char(book_text)
+def print_results(num_words, num_chars):
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
     print(f"Found {num_words} total words")
-    print(num_chars)
+    print("--------- Character Count -------")
+    for item in num_chars:
+        if item["char"].isalpha():
+            print(f"{item["char"]}: {item["num"]}")
+    print("============= END ===============")
+
+
+def main():
+    #gets frankenstein text from dir books
+    book_text = get_book_text("books/frankenstein.txt")
+    #get number of words in the text
+    num_words = get_num_words(book_text)
+    #get the number of each character in the text
+    num_chars = get_num_char(book_text)
+    num_chars = sort_dictionary(num_chars)
+
+    #print results
+    print_results(num_words, num_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -1,13 +1,42 @@
+#function takes the string of text and returns the number of words in the text
 def get_num_words(book_text):
+    #get a list of all the words using the split function
     words = book_text.split()
+    #returns the length of the list
     return len(words)
 
+#function takes the string of text and returns a dict of all the characters
+#and their number of appearences in the text
 def get_num_char(book_text):
+    #create empty dict
     num_char = {}
+
+    #for each char
     for c in book_text:
+        #convert char to lower if necessary 
         c = c.lower()
+        #if the char is already in our dict, add 1; else initialize it with a count of 1
         if c in num_char:
             num_char[c] += 1
         else:
             num_char[c] = 1
+    #return the dict
     return num_char
+
+def sort_on(items):
+    return items["num"]
+
+def sort_dictionary(dict):
+    sorted_list = []
+    for char in dict:
+        new_dict = {"char": char, "num": dict[char]}
+        sorted_list.append(new_dict)
+    
+    sorted_list.sort(reverse=True, key=sort_on)
+
+    return sorted_list
+
+    
+
+        
+
