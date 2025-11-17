7bf50d7ee4e44af8396d3bdc1be70a7241acdb78
boorivard <boorivard@comcast.net>
2025-11-16 19:26:06 -0500

    added get_num_char

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from stats import get_num_words
+from stats import get_num_char
 
 def get_book_text(path_to_file):
     with open(path_to_file) as f:
@@ -8,6 +9,8 @@ def get_book_text(path_to_file):
 def main():
     book_text = get_book_text("books/frankenstein.txt")
     num_words = get_num_words(book_text)
+    num_chars = get_num_char(book_text)
     print(f"Found {num_words} total words")
+    print(num_chars)
 
 main()
--- stats.py
+++ stats.py
@@ -1,3 +1,13 @@
 def get_num_words(book_text):
     words = book_text.split()
     return len(words)
+
+def get_num_char(book_text):
+    num_char = {}
+    for c in book_text:
+        c = c.lower()
+        if c in num_char:
+            num_char[c] += 1
+        else:
+            num_char[c] = 1
+    return num_char
